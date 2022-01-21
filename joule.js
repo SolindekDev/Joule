5b6002b4cdc864d0c30dea76543ed5a4a6dcffd1
SolindekDev <83218819+SolindekDev@users.noreply.github.com>
2022-01-21 21:40:47 +0100

    Update joule.js

--- joule.js
+++ joule.js
@@ -17,36 +17,152 @@ const TYPES = {
 	EQUALS:		 "EQUALS"
 }
 
+const colorReset = "\033[0m"
+const colorRed    = "\033[31m"
+const colorGreen  = "\033[32m"
+const colorYellow = "\033[33m"
+const colorBlue   = "\033[34m"
+const colorPurple = "\033[35m"
+const colorCyan   = "\033[36m"
+const colorWhite  = "\033[37m"
+
+async function exists(path, include, token) {
+	try {
+		const data = fs.readFileSync(path, 'UTF-8');
+		return true
+	} catch (e) {
+		if (include) {
+			console.log(`${colorRed}IncludeError: ${colorWhite}${token.filename}:${token.row}:${token.col}${colorReset} no such file found ${colorYellow}"${path}"${colorReset}`)
+			process.exit(0)
+		} else {
+			console.log(`${colorRed}FileError:${colorWhite} ${path} ${colorReset}this file not exists\n`)
+			help()
+		}
+	}
+}
+
 function compile(only_compile, only_o, parser) {
 	var out_code_data = `section .data\n`
+	var out_code_functions = ``
 	var out_code = START_CODE + "\n";
 	var code_var = 1
+
+	var open_function = 0
 	console.log("[COMPILING] Compiling into assembly code")
 	parser.program.forEach((token) => {
 		if (token.code == 1) {
-			const name_for_str = `strPUTSjsn1kla${Math.floor(Math.random() * 20000)}`
-			out_code_data += `	${name_for_str} db "${token.text}"\n`
-			out_code += `	mov eax, 4\n`
-        	out_code += `	mov ebx, 1\n`
-        	out_code += `	mov ecx, ${name_for_str}\n`
-        	out_code += `	mov edx, ${token.text.length}\n`
-        	out_code += `	int 0x80\n\n`
-		} else if (token.code == 2) {
-			out_code += "	mov eax, 1\n"
-			out_code += "	mov ebx, 0\n"
-			out_code += "	int 0x80\n\n"
-		} else if (token.code == 3) {
-			out_code += "	hlt\n\n"
-		} else if (token.code == 4) {
-			out_code += "	; CODE INPUT\n"
-			out_code += token.text.split("\\n").join("\n") + "\n\n"
-		} else if (token.code == 5) {
-			if (token.type == TYPES.INT) {
-				out_code += `	mov byte [${code_var}h], ${token.value}\n\n`
+			if (open_function == 0) {
+				const name_for_str = `strPUTSjsn1kla${Math.floor(Math.random() * 20000)}`
+				var newlines = 0
+				for (let i = 0; i < token.text.length; i++) {
+					if (token.text[i] == "\\" && token.text[i+1] == "n") {
+						newlines++
+					}
+				}
+				if (newlines == 0) {
+					out_code_data += `	${name_for_str} db "${token.text}"`
+				} else {
+					out_code_data += `	${name_for_str} db "${token.text.split("\\n").join("")}"`
+					for (let j = 0; j < newlines; j++) {
+						out_code_data += `,0xA,0xD,""`
+					}
+				}
+				out_code_data += "\n"
+				out_code_data += `	len${name_for_str} equ $-${name_for_str}\n`
+				out_code += `	mov eax, 4\n`
+      	out_code += `	mov ebx, 1\n`
+      	out_code += `	mov ecx, ${name_for_str}\n`
+      	out_code += `	mov edx, ${token.text.length}\n`
+      	out_code += `	int 0x80\n\n`
 			} else {
-				const name_for_strr = `strVARn2h1s${Math.floor(Math.random() * 20000)}`
-				out_code_data += `	${name_for_strr} db "${token.value}"\n`
-				out_code += `	mov byte [${code_var}h], ${name_for_strr}\n\n`
+				const name_for_str = `strPUTSjsn1kla${Math.floor(Math.random() * 20000)}`
+				var newlines = 0
+				for (let i = 0; i < token.text.length; i++) {
+					if (token.text[i] == "\\" && token.text[i+1] == "n") {
+						newlines++
+					}
+				}
+				if (newlines == 0) {
+					out_code_data += `	${name_for_str} db "${token.text}"`
+				} else {
+					out_code_data += `	${name_for_str} db "${token.text.split("\\n").join("")}"`
+					for (let j = 0; j < newlines; j++) {
+						out_code_data += `,0xA,0xD,""`
+					}
+				}
+				out_code_data += "\n"
+				out_code_data += `	len${name_for_str} equ $-${name_for_str}\n`
+				out_code_functions += `	mov eax, 4\n`
+      	out_code_functions += `	mov ebx, 1\n`
+      	out_code_functions += `	mov ecx, ${name_for_str}\n`
+      	out_code_functions += `	mov edx, ${token.text.length}\n`
+      	out_code_functions += `	int 0x80\n\n`
+			}
+		} else if (token.code == 2) {
+			if (open_function == 0) {
+				out_code += "	mov eax, 1\n"
+				out_code += "	mov ebx, 0\n"
+				out_code += "	int 0x80\n\n"
+			} else {
+				out_code_functions += "	mov eax, 1\n"
+				out_code_functions += "	mov ebx, 0\n"
+				out_code_functions += "	int 0x80\n\n"
+			}
+		} else if (token.code == 3) {
+			if (open_function == 0) {
+				out_code += "	hlt\n\n"
+			} else {
+				out_code_functions += "	hlt\n\n"
+			}
+		} else if (token.code == 4) {
+			if (open_function == 0) {
+				out_code += "	; CODE INPUT\n"
+				out_code += token.text.split("\\n").join("\n") + "\n\n"
+			} else {
+				out_code_functions += "	; CODE INPUT\n"
+				out_code_functions += "	" + token.text.split("\\n").join("\n") + "\n\n"
+			}
+		} else if (token.code == 5) {
+			// if (open_function == 0) {
+			// 	if (token.type == TYPES.INT) {
+			// 		out_code += `	mov byte [${code_var}h], ${token.value}\n\n`
+			// 	} else {
+			// 		const name_for_strr = `strVARn2h1s${Math.floor(Math.random() * 20000)}`
+			// 		out_code_data += `	${name_for_strr} db "${token.value}"\n`
+			// 		out_code += `	mov byte [${code_var}h], ${name_for_strr}\n\n`
+			// 	}
+			// } else {
+			// 	if (token.type == TYPES.INT) {
+			// 		out_code_functions += `	mov byte [${code_var}h], ${token.value}\n\n`
+			// 	} else {
+			// 		const name_for_strr = `strVARn2h1s${Math.floor(Math.random() * 20000)}`
+			// 		out_code_data += `	${name_for_strr} db "${token.value}"\n`
+			// 		out_code_functions += `	mov byte [${code_var}h], ${name_for_strr}\n\n`
+			// 	}
+			// }
+			if (open_function == 0) {
+				if (token.type == TYPES.INT) {
+					out_code_data += `	${token.name} dq ${token.value}\n`
+				} else if (token.type == TYPES.FLOAT) {
+					out_code_data += `	${token.name} dd ${token.value}\n`
+				} else {
+					out_code_data += `	${name_for_strr} db "${token.value}"\n`
+				}
+			} else {
+
+			}
+		}
+		 else if (token.code == 6) {
+			out_code_functions += `\n${token.name}:\n\n`
+			open_function = 1
+		} else if (token.code == 7) {
+			open_function = 0
+			out_code_functions += "	ret\n"
+		} else if (token.code == 8) {
+			if (open_function == 0) {
+				out_code += `	call ${token.name}\n\n`
+			} else {
+				out_code_functions += `	call ${token.name}\n\n`
 			}
 		}
 	})
@@ -55,7 +171,9 @@ function compile(only_compile, only_o, parser) {
 	out_code += "	mov ebx, 0\n"
 	out_code += "	int 0x80\n\n"
 
+	out_code += out_code_functions + "\n"
 	out_code += out_code_data + "\n"
+
 
 	console.log("[FILE] Writting assembly code into file")
 
@@ -86,13 +204,13 @@ function compile(only_compile, only_o, parser) {
 						if (err) return console.log(`Error ${err.message}`)
 						if (ster) return console.log(`StdError ${ster}`)
 
-						console.log("[START FILE] Start the executable file\n")
+						console.log("[START FILE] Start the executable file")
 
 						exec("rm out.* && ./a.out", (err, stou, ster) => {
 							if (err) return console.log(`Error ${err.message}`)
 							if (ster) return console.log(`StdError ${ster}`)
 
-							console.log(stou + "\n")
+							console.log(stou + "")
 						})
 					})
 				}
@@ -132,18 +250,9 @@ function readfile(path) {
 
 // console.log(`${path}:${row}:${col} ${letter}`)
 
-const colorReset = "\033[0m"
-const colorRed    = "\033[31m"
-const colorGreen  = "\033[32m"
-const colorYellow = "\033[33m"
-const colorBlue   = "\033[34m"
-const colorPurple = "\033[35m"
-const colorCyan   = "\033[36m"
-const colorWhite  = "\033[37m"
-
 const CONSTANTS = {
 	NUMBERS: "0123456789",
-	LETTERS: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_?"
+	LETTERS: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_?@"
 }
 
 function lexer(path) {
@@ -310,9 +419,10 @@ const KEYWORDS = {
 	EXIT: "exit",
 	HALT: "halt",
 	ASM: "__asm__",
-	DEF: "def",
+	FN: "fn",
 	CONST: "const",
-	END: "end"
+	END: "end",
+	INCLUDE: "@include"
 }
 
 const PRINT = (text, pos) => { return({ code: 1, text: text, pos: pos }) }
@@ -320,6 +430,9 @@ const EXIT = (pos) => { return({ code: 2, pos: pos }) }
 const HALT = (pos) => { return({ code: 3, pos: pos }) }
 const ASM = (text ,pos) => { return({ code: 4, text: text, pos: pos }) }
 const VARIABLE = (name, value, type ,pos) => { return({ code: 5, value: value, name: name, type: type, pos: pos }) }
+const FN = (name, pos) => { return({ code: 6, name: name, pos: pos }) }
+const END = (pos) => { return({ code: 7, pos: pos }) }
+const JMP_FN = (name, pos) => { return({ code: 8, pos: pos, name: name }) }
 
 const program = [
 
@@ -334,16 +447,37 @@ class Variables {
 }
 
 class Function {
-	constructor(name, argument) {
+	constructor(name) {
 		this.name = name;
-		this.argument = argument
 	}
 }
 
-function parser(tokens) {
+
+function debuger(tokens) {
+	console.log(`----------------| ${colorRed}Debug${colorReset} |----------------\n`)
+	tokens.program.forEach((token) => {
+		if (token.code == 2) {
+			console.log(`${colorYellow}${token.pos.filename}:${token.pos.row}:${token.pos.col}${colorReset} | ${colorBlue}${token.pos.value}${colorReset} | ${colorPurple}${token.code}${colorReset} |`)
+		} else if (token.code == 3) {
+			console.log(`${colorYellow}${token.pos.filename}:${token.pos.row}:${token.pos.col}${colorReset} | ${colorBlue}${token.pos.value}${colorReset} | ${colorPurple}${token.code}${colorReset} |`)
+		} else if (token.code == 1) {
+			console.log(`${colorYellow}${token.pos.filename}:${token.pos.row}:${token.pos.col}${colorReset} | ${colorBlue}${token.pos.value}${colorReset} | ${colorPurple}${token.code}${colorReset} | text: ${colorCyan}${token.text}${colorReset}`)
+		} else if (token.code == 4) {
+			console.log(`${colorYellow}${token.pos.filename}:${token.pos.row}:${token.pos.col}${colorReset} | ${colorBlue}${token.pos.value}${colorReset} | ${colorPurple}${token.code}${colorReset} | text: ${colorCyan}${token.text}${colorReset}`)
+		} else if (token.code == 5) {
+			console.log(`${colorYellow}${token.pos.filename}:${token.pos.row}:${token.pos.col}${colorReset} | ${colorBlue}${token.pos.value}${colorReset} | ${colorPurple}${token.code}${colorReset} | type: ${colorCyan}${token.type}${colorReset} | name: ${colorPurple}${token.name}${colorReset} | value: ${colorCyan}${token.value}${colorReset} |`)
+		}
+	})
+	console.log(`\n-----------------------------------------`)
+
+}
+
+async function parser(tokens) {
 
 	var freeze = 0;
 	var error = 0;
+
+	var functionOpen = 0;
 
 	var variables = [];
 	var names = [];
@@ -371,6 +505,34 @@ function parser(tokens) {
 						error+=1;
 					}
 				}
+			} else if (token.value == KEYWORDS.FN) {
+				if (tokens[i+1] == undefined) {
+					console.log(`${colorRed}SyntaxError: ${colorWhite}${tokens[i+1].filename}:${tokens[i+1].row}:${tokens[i+1].col}${colorReset} Name of function is required`)
+					error+=1;
+				} else {
+					if (functionOpen == 1) {
+						console.log(`${colorRed}TypeError: ${colorWhite}${token.filename}:${token.row}:${token.col}${colorReset} You are opening new function when the old function is not closed with ${colorYellow}end${colorReset} keyword!`)
+						freeze+=1
+						error+=1
+					} else {
+						if (tokens[i+1].type == TYPES.IDENTIFIER) {
+								if (names.includes(tokens[i+1].value)) {
+									console.log(`${colorRed}TypeError: ${colorWhite}${token.filename}:${token.row}:${token.col}${colorReset} ${colorYellow}${tokens[i+1].value}${colorReset} is already defined`)
+									freeze+=1
+									error+=1
+								} else {
+									program.push(FN(tokens[i+1].value, tokens[i+1]))
+									names.push(tokens[i+1].value)
+									functions.push(new Function(tokens[i+1].value))
+									functionOpen=1
+									freeze+=1
+								}
+							} else {
+								console.log(`${colorRed}SyntaxError: ${colorWhite}${tokens[i+1].filename}:${tokens[i+1].row}:${tokens[i+1].col}${colorReset} Name of function need to be type ${colorYellow}IDENTIFIER${colorReset} no ${colorYellow}${tokens[i+1].type}${colorReset}`)
+								error+=1;
+							}
+						}
+					}
 			} else if (token.value == KEYWORDS.EXIT) {
 				program.push(EXIT(token))
 			} else if (token.value == KEYWORDS.HALT) {
@@ -441,30 +603,34 @@ function parser(tokens) {
 						error+=1;
 					}
 				}
+			} else if (token.value == KEYWORDS.END) {
+				if (functionOpen == 1) {
+					functionOpen = 0
+					program.push(END(token))
+				} else {
+					console.log(`${colorRed}TypeError: ${colorWhite}${token.filename}:${token.row}:${token.col}${colorReset} ${colorYellow}end${colorReset} keyword is used when we want to close the function`)
+					error+=1;
+				}
+			} else if (token.value == KEYWORDS.INCLUDE) {
+				freeze+=1
 			} else {
 				if (names.includes(token.value)) {
 					if (variables.find(variable => variable.name == token.value) == undefined) {
 						if (functions.find(_function => _function.name == token.value) == undefined) {
-							console.log(`${colorRed}TypeError: ${colorWhite}${token.filename}:${token.row}:${token.col}${colorReset} ${colorYellow}${tokens[i+1].value}${colorReset} is not defined`)
+							console.log(`${colorRed}TypeError: ${colorWhite}${token.filename}:${token.row}:${token.col}${colorReset} ${colorYellow}${token.value}${colorReset} is not defined`)
 							error+=1;
 						} else {
-							console.log("Function is defined")
+							program.push(JMP_FN(token.value, token))
 						}
 					} else {
 						const _var = variables.find(variable => variable.name == token.value)
 						const _freezer = 0
 						const _next = 0
 
-						if (_var.type == TYPES.INT) {
-							// if (tokens[i+_next])
-						} else if (_var.type == TYPES.FLOAT) {
 
-						} else {
-
-						}
 					}
 				} else {
-					console.log(`${colorRed}TypeError: ${colorWhite}${token.filename}:${token.row}:${token.col}${colorReset} ${colorYellow}${token.value}${colorReset} keyword or variable not found`)
+					console.log(`${colorRed}SyntaxError: ${colorWhite}${token.filename}:${token.row}:${token.col}${colorReset} ${colorYellow}${token.value}${colorReset} keyword, function or even variable not found because it's not defined`)
 					error+=1;
 				}
 			}
@@ -476,33 +642,53 @@ function parser(tokens) {
 	return {freeze:freeze, error:error, program:program}
 }
 
-async function exists(path) {
-	try {
-		const data = fs.readFileSync(path, 'UTF-8');
-		return true
-	} catch (e) {
-		console.log(`${colorRed}FileError:${colorWhite} ${path} ${colorReset}this file not exists\n`)
-		help()
-	}
-}
+// else if (token.value == KEYWORDS.INCLUDE) {
+// 	if (tokens[i+1] == undefined) {
+// 		console.log(`${colorRed}IncludeError: ${colorWhite}${token.filename}:${token.row}:${token.col}${colorReset} file to include not given`)
+// 		error+=1;
+// 	} else {
+// 		if (tokens[i+1].type == TYPES.STRING) {
+// 			exists(tokens[i+1].value, true, tokens[i+1])
+//
+// 			var tokensInclude = lexer(tokens[i+1].value);
+// 			console.log(tokensInclude)
+// 		} else {
+// 			console.log(`${colorRed}IncludeError: ${colorWhite}${token.filename}:${token.row}:${token.col}${colorReset} file name to include need to be ${colorYellow}STRING${colorReset} type no ${colorYellow}${tokens[i+1].type}${colorReset}`)
+// 			error+=1;
+// 		}
+// 	}
+// }
 
-function debuger(tokens) {
-	console.log(`----------------| ${colorRed}Debug${colorReset} |----------------\n`)
-	tokens.program.forEach((token) => {
-		if (token.code == 2) {
-			console.log(`${colorYellow}${token.pos.filename}:${token.pos.row}:${token.pos.col}${colorReset} | ${colorBlue}${token.pos.value}${colorReset} | ${colorPurple}${token.code}${colorReset} |`)
-		} else if (token.code == 3) {
-			console.log(`${colorYellow}${token.pos.filename}:${token.pos.row}:${token.pos.col}${colorReset} | ${colorBlue}${token.pos.value}${colorReset} | ${colorPurple}${token.code}${colorReset} |`)
-		} else if (token.code == 1) {
-			console.log(`${colorYellow}${token.pos.filename}:${token.pos.row}:${token.pos.col}${colorReset} | ${colorBlue}${token.pos.value}${colorReset} | ${colorPurple}${token.code}${colorReset} | text: ${colorCyan}${token.text}${colorReset}`)
-		} else if (token.code == 4) {
-			console.log(`${colorYellow}${token.pos.filename}:${token.pos.row}:${token.pos.col}${colorReset} | ${colorBlue}${token.pos.value}${colorReset} | ${colorPurple}${token.code}${colorReset} | text: ${colorCyan}${token.text}${colorReset}`)
-		} else if (token.code == 5) {
-			console.log(`${colorYellow}${token.pos.filename}:${token.pos.row}:${token.pos.col}${colorReset} | ${colorBlue}${token.pos.value}${colorReset} | ${colorPurple}${token.code}${colorReset} | type: ${colorCyan}${token.type}${colorReset} | name: ${colorPurple}${token.name}${colorReset} | value: ${colorCyan}${token.value}${colorReset} |`)
+// Yeah it's workinggggggg :DDDDD
+function include_files(tokens) {
+	for (let i = 0; i < tokens.length; i++) {
+		const token = tokens[i]
+
+		if (token.value == KEYWORDS.INCLUDE && token.type == TYPES.IDENTIFIER) {
+			if (tokens[i+1] == undefined) {
+				console.log(`${colorRed}IncludeError: ${colorWhite}${token.filename}:${token.row}:${token.col}${colorReset} file to include not given`)
+				error+=1;
+			} else {
+				if (tokens[i+1].type == TYPES.STRING) {
+					exists(tokens[i+1].value, true, tokens[i+1])
+					var tokensInclude = lexer(tokens[i+1].value);
+					if (i-1 == -1) {
+						tokens.splice(0, i+2)
+					} else {
+						tokens.splice(i-1, i+2)
+					}
+					i-=1
+					const addTokens = tokensInclude.concat(tokens)
+					tokens = addTokens
+					console.log(tokens)
+				} else {
+					console.log(`${colorRed}IncludeError: ${colorWhite}${token.filename}:${token.row}:${token.col}${colorReset} file name to include need to be ${colorYellow}STRING${colorReset} type no ${colorYellow}${tokens[i+1].type}${colorReset}`)
+					error+=1;
+				}
+			}
 		}
-	})
-	console.log(`\n-----------------------------------------`)
-
+	}
+	return tokens
 }
 
 async function main() {
@@ -521,8 +707,9 @@ async function main() {
 		exists(process.argv[1]);
 
 		var tokens = lexer(process.argv[1]);
-		var tokensParsed = parse_numbers(tokens)
-		var parsert = parser(tokensParsed);
+		var tokens_parsed = parse_numbers(tokens)
+		var includes_tokens = include_files(tokens_parsed);
+		var parsert = parser(includes_tokens);
 
 		if (parsert.error == 0) {
 			if (debug == true) {
